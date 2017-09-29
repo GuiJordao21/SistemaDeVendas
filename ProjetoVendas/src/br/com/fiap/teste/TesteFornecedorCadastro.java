5ed6c9b98b3b12c433263e9fddfb37b3294425be
logonrm <logonrm@PA05MICRO29.fiap.com.br>
2017-09-29 11:53:17 -0300

    TESTES NÃO REALIZADOS, PRECISAM SER FEITOS AINDA.

--- ProjetoVendas/src/br/com/fiap/teste/TesteFornecedorCadastro.java
+++ ProjetoVendas/src/br/com/fiap/teste/TesteFornecedorCadastro.java
@@ -21,7 +21,40 @@ public class TesteFornecedorCadastro {
 		Dados.mensagem(dao.cadastrar(fb));
 		
 		dao.consultarNome(Dados.texto("Qual o nome do fornecedor"));
-				
+		
+		char opt=Dados.texto("Qual dado deseja atualizar?"
+				+ " -NOME-"
+				+ "/n-EMAIL-"
+				+ "/n-CNPJ-"
+				+ "/n-RAZÃO SOCIAL-").toUpperCase().charAt(0);
+		
+		if (opt=='N'){
+			
+			dao.atualizar(Dados.inteiro("Insira o código"),
+					"NM_FORNECEDOR",
+					Dados.texto("Insira o novo nome:"));
+			
+		}else if(opt=='E'){
+			
+			dao.atualizar(Dados.inteiro("Insira o código"),
+					"DS_EMAIL",
+					Dados.texto("Insira o novo email:"));
+			
+		}else if(opt=='C'){
+
+			dao.atualizar(Dados.inteiro("Insira o código"),
+					"NR_CNPJ",
+					Dados.longo("Insira o novo CNPJ:"));
+			
+		}else if(opt=='R'){
+			
+			dao.atualizar(Dados.inteiro("Insira o código"),
+					"DS_RAZAO_SOCIAL",
+					Dados.texto("Insira a nova Razão Social:"));
+			
+		}else{
+			Dados.mensagem("Opção inválida.");
+		}
 
 	}
 
