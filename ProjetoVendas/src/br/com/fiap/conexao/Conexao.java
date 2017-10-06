7d719ba4531a638c336b5829ea365a16229582a9
logonrm <logonrm@PA05MICRO29.fiap.com.br>
2017-10-06 08:25:09 -0300

    Conexão alterada para se trabalhar co arquivos txt

--- ProjetoVendas/src/br/com/fiap/conexao/Conexao.java
+++ ProjetoVendas/src/br/com/fiap/conexao/Conexao.java
@@ -5,28 +5,34 @@ import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class Conexao {
+public final class Conexao {
 	
-	private static String lerArq()throws Exception{
-		
-		FileReader arquivo=new FileReader("c:\\conexaoBanco.txt");
-		BufferedReader dados=new BufferedReader(arquivo);
-		String resultado="";
-		while(dados.ready()){ 
-			resultado+=dados.readLine();
+	private static Conexao conexao=null;
+	
+	public static Conexao controlarInstancia(){
+		if (conexao==null) {
+			conexao=new Conexao();
 		}
+		return conexao;
+	}
+	
+	public Connection getConnection(String usuario, String senha) throws Exception{
+		
+		//FileReader arquivo=new FileReader("conexao/conexao.txt");
+		FileReader arquivo=new FileReader(System.getProperty("user.dir")+"/conexao/conexao.txt");
+		BufferedReader dados=new BufferedReader(arquivo);
+		
+		String url=dados.readLine();
+		//retiramos a criação das variaveis usuario e senha
+		
+		if (url.indexOf("oracle")>0){
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}else if(url.indexOf("mysql")>0){
+			Class.forName("com.mysql.jdbc.Driver");
+		}
+		
 		dados.close();
-		arquivo.close();
-		
-		return resultado;
-		
+		return DriverManager.getConnection(url,usuario,senha);
 	}
 
-	
-	public Connection getconnection() throws Exception{
-		
-		return DriverManager.getConnection(lerArq());
-		
-	}
-	
 }
